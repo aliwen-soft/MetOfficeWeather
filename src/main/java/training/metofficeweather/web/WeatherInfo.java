a16c614d535f08d8a9fc56eaee2c31c72edef0b7
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-23 20:17:05 +0000

    I can print out a list on the webpage!

--- src/main/java/training/metofficeweather/web/WeatherInfo.java
+++ src/main/java/training/metofficeweather/web/WeatherInfo.java
@@ -1,13 +1,28 @@
 package training.metofficeweather.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import training.metofficeweather.MetAPIReader;
+
 import java.util.List;
 
 public class WeatherInfo {
     private final String locationId;
-    private List<String> returned;
+    private List<String> weatherData;
 
     public WeatherInfo(String locationId) {
         this.locationId = locationId;
+    }
+
+    public void populateData(){
+        try {
+            this.weatherData = MetAPIReader.returnWeatherFromId(locationId);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> getWeatherData() {
+        return weatherData;
     }
 
     public String getLocationId() {
