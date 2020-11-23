083fb3f9ffac9d0b4eb0d11b67ae16deb5edf764
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-23 10:27:15 +0000

    set up without key

--- src/main/java/training/metofficeweather/Main.java
+++ src/main/java/training/metofficeweather/Main.java
@@ -1,7 +1,18 @@
 package training.metofficeweather;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        Client client = ClientBuilder.newClient();
+        String name = client.target("")
+                .request(MediaType.TEXT_PLAIN)
+                .get(String.class);
+
+        Client newClient = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
     }
 }	
